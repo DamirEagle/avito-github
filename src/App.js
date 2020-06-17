caea3ee015d4bc4bd572cea877e5ca6a948c9931
Zulpukaarov Damirbek <Ashimovd1908@gmail.com>
2020-06-18 02:17:21 +0300

    seems like works

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React, { Component } from 'react';
-import ListRepos from './listRepos/listRepos';
-import Search from './search/search';
-import Loader from './Loader/Loader';
-import { avito, per_page } from './constants';
-import PaginationList from './paginationList/paginationList';
+import React, { Component } from "react";
+import ListRepos from "./listRepos/listRepos";
+import Search from "./search/search";
+import Loader from "./Loader/Loader";
+import { avito, per_page } from "./constants";
+import PaginationList from "./paginationList/paginationList";
 
 class App extends Component {
   constructor(props) {
@@ -14,86 +14,119 @@ class App extends Component {
       search: "",
       currentPage: 1,
       totalCount: 0,
-      didMount: false
+      didMount: false,
     };
   }
   render() {
     return (
       <div>
-        {
-          localStorage.getItem("limit") * 1 > 1 ?
-            <Search onInputChange={this.onInputChange} search={this.state.search} /> :
-            <div>Лимит достигнут, подождите</div>
-        }
-        {
-          this.state.isLoaded ?
-            <div>
-              <ListRepos data={this.state.data} />
-              {
-                localStorage.getItem("limit") * 1 > 1 ?
-                  <PaginationList pageChangedFn={this.pageChanged} currentPage={this.state.currentPage} totalCount={this.state.totalCount}></PaginationList> :
-                  <div>Лимит достигнут, подождите</div>
-              }
-            </div> : <Loader></Loader>
-        }
+        {localStorage.getItem("limit") * 1 > 1 ? (
+          <Search
+            onInputChange={this.onInputChange}
+            search={this.state.search}
+          />
+        ) : (
+          <div>Лимит достигнут, подождите</div>
+        )}
+        {this.state.isLoaded ? (
+          <div>
+            <ListRepos data={this.state.data} />
+            {localStorage.getItem("limit") * 1 > 1 ? (
+              <PaginationList
+                pageChangedFn={this.pageChanged}
+                currentPage={this.state.currentPage}
+                totalCount={this.state.totalCount}
+              ></PaginationList>
+            ) : (
+              <div>Лимит достигнут, подождите</div>
+            )}
+          </div>
+        ) : (
+          <Loader></Loader>
+        )}
       </div>
     );
   }
 
   onInputChange = async (search) => {
-    if (localStorage.getItem("limit") * 1 > 1) {
-      if (this.state.didMount) {
-        await this.setState({ ...this.state, isLoaded: false, currentPage: 1, search })
-        localStorage.setItem("currentPage", 1)
-        this.githubQuerry();
-      }
-    }
-  }
-  newLimit = async () => {
-    localStorage.setItem("limit", 10)
-    this.setState({ ...this.state })
-  }
-  async componentDidMount() {
-    if (localStorage.getItem("limit") === null) {
-      localStorage.setItem("limit", 10)
-    }
-    if (localStorage.getItem("limit") * 1 > 1) {
-      await this.setState({ ...this.state, didMount: false, currentPage: localStorage.getItem("currentPage") * 1, search: localStorage.getItem("search") })
+    if (this.state.didMount) {
+      await this.setState({
+        ...this.state,
+        isLoaded: false,
+        currentPage: 1,
+        search,
+      });
+      localStorage.setItem("currentPage", 1);
       this.githubQuerry();
-      setTimeout(() => {
-        this.setState({ ...this.state, didMount: true })
-      }, 3000)
-    } else {
-      this.setState({ ...this.state, didMount: false })
     }
+  };
+  newLimit = async () => {
+    if (!this.state.isLoaded) {
+    }
+    await this.setState({ ...this.state, didMount: false });
+    localStorage.setItem("limit", 10);
+    this.setState({ ...this.state });
+    setTimeout(() => {
+      this.setState({ ...this.state, didMount: true });
+    }, 3000);
+  };
+
+  async componentDidMount() {
+    await this.setState({
+      ...this.state,
+      didMount: false,
+      currentPage: localStorage.getItem("currentPage") * 1,
+      search: localStorage.getItem("search"),
+    });
+    await this.githubQuerry();
+    setTimeout(() => {
+      this.setState({ ...this.state, didMount: true });
+    }, 3000);
+    setInterval(this.newLimit, 60000);
   }
   githubQuerry = () => {
-    try {
-      fetch(`https://api.github.com/search/repositories?q=${this.state.search ? this.state.search : avito}&page=${this.state.currentPage}&per_page=${per_page}&sort=stars&order=desc`)
-        .then(res => {
-          localStorage.setItem("limit", res.headers.get("x-ratelimit-remaining"))
-          console.log(res.headers.get("x-ratelimit-remaining"))
-          return res.json()
+    if (localStorage.getItem("limit") > 1) {
+      fetch(
+        `https://api.github.com/search/repositories?q=${
+          this.state.search ? this.state.search : avito
+        }&page=${
+          this.state.currentPage
+        }&per_page=${per_page}&sort=stars&order=desc`
+      )
+        .then((res) => {
+          localStorage.setItem(
+            "limit",
+            res.headers.get("x-ratelimit-remaining")
+          );
+          console.log(res.headers.get("x-ratelimit-remaining"));
+          return res.json();
         })
-        .then(async data => {
-          await this.setState({ ...this.state, data: data.items, isLoaded: true, totalCount: data.total_count })
-          setInterval(this.newLimit, 60000);
-        })
-      console.log("сработал githubQuerry")
-    } catch (e) {
-
+        .then(async (data) => {
+          await this.setState({
+            ...this.state,
+            data: data.items,
+            isLoaded: true,
+            totalCount: data.total_count,
+          });
+        });
+      console.log("сработал githubQuerry");
     }
-  }
+    setTimeout(() => {
+      localStorage.setItem("limit", 10);
+      this.githubQuerry();
+    }, 60000);
+  };
   pageChanged = async (newPage) => {
-    if (localStorage.getItem("limit") * 1 > 1) {
-      if (this.state.currentPage !== newPage) {
-        localStorage.setItem("currentPage", String(newPage))
-
-        await this.setState({ ...this.state, isLoaded: false, currentPage: newPage })
-        this.githubQuerry();
-      }
+    if (this.state.currentPage !== newPage) {
+      localStorage.setItem("currentPage", String(newPage));
+      await this.setState({
+        ...this.state,
+        isLoaded: false,
+        currentPage: newPage,
+      });
+      this.githubQuerry();
     }
-  }
+  };
 }
 
 export default App;
